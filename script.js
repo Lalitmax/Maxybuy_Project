1cb1402fdcc82e5f1b5aedfe3de04e877c993095
lalitmax <lalitmaxy@gmail.com>
2023-04-23 20:06:37 +0530

    add product name Maxybuy

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
+// Registeration Event
 const registerLink = document.querySelector('.register-link');
-
-const btnPopup = document.querySelector('.btn-popup');
-
-const iconClose = document.querySelector('.icon-close');
-
 registerLink.addEventListener('click' ,()=>{
     wrapper.classList.add('active');
 });
 
+// Login Event
+const loginLink = document.querySelector('.login-link');
 loginLink.addEventListener('click' ,()=>{
     wrapper.classList.remove('active');
 });
 
+// Button pop up Event
+const btnPopup = document.querySelector('.btn-popup');
 btnPopup.addEventListener('click' ,()=>{
     wrapper.classList.add('active-popup');
 });
 
+// Close icon Event
+const iconClose = document.querySelector('.icon-close');
 iconClose.addEventListener('click' ,()=>{
     wrapper.classList.remove('active-popup');
 });
