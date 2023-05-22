39eab3235ec2953ab722e55e9cd93725f8be5175
lalitmax <lalitmaxy@gmail.com>
2023-05-22 23:57:34 +0530

    adding services details

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ const iconClose = document.querySelector('.icon-close');
 iconClose.addEventListener('click' ,()=>{
     wrapper.classList.remove('active-popup');
 });
+
+function myFunction() {
+    var x = document.getElementById("click");
+    x.className = "show";
+  
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);}
